24337987bd56cb86798d01918721567a1eaf89e9
Yamikani Phiri <yamikanidj@gmail.com>
2021-12-16 11:51:25 +0200

    added class first oop code

---
+++ class-first-oop-code/Circle.java
@@ -0,0 +1,13 @@
+class Circle{
+    double radius=10;
+    final double PI = 3.14;
+    Circle(){
+    }
+    // calc area of circle
+    public double getArea(){
+        double area = radius * radius * PI;
+        return area;
+    }
+
+
+}
---
+++ class-first-oop-code/CircleArea.java
@@ -0,0 +1,11 @@
+public class CircleArea{
+    public static void main(String[] args){
+        System.out.println("working");
+        //creating another class
+        //check if connnecting
+        Circle c1 = new Circle();
+        System.out.println(c1.radius);
+        System.out.println("areas is"+c1.getArea());
+        //use the created class to calc area
+    }
+}
